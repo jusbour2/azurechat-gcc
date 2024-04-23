0ff72e4a8ae4f907593b048ad18c307d63af871c
Justin Harbour <github2@justinharbour.com>
2024-04-23 00:21:12 -0400

    mirror commercial now

--- src/features/auth-page/auth-api.ts
+++ src/features/auth-page/auth-api.ts
@@ -4,65 +4,65 @@ import { Provider } from "next-auth/providers/index";
 import OktaProvider from "next-auth/providers/okta";
 
 const configureIdentityProvider = () => {
-  const providers: Array<Provider> = [];
+    const providers: Array<Provider> = [];
 
-  if (
-      process.env.AZURE_AD_CLIENT_ID &&
-      process.env.AZURE_AD_CLIENT_SECRET &&
-      process.env.AZURE_AD_TENANT_ID
-  ) {
-    providers.push(
-        AzureADProvider({
-          clientId: process.env.AZURE_AD_CLIENT_ID!,
-          clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-          tenantId: process.env.AZURE_AD_TENANT_ID!,
-          async profile(profile) {
-            const newProfile = {
-              ...profile,
-              id: profile.sub,
-              isAdmin: false, // Regular users from Azure AD
-            };
-            return newProfile;
-          },
-        })
-    );
-  }
+    if (
+        process.env.AZURE_AD_CLIENT_ID &&
+        process.env.AZURE_AD_CLIENT_SECRET &&
+        process.env.AZURE_AD_TENANT_ID
+    ) {
+        providers.push(
+            AzureADProvider({
+                clientId: process.env.AZURE_AD_CLIENT_ID!,
+                clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+                tenantId: process.env.AZURE_AD_TENANT_ID!,
+                async profile(profile) {
+                    const newProfile = {
+                        ...profile,
+                        id: profile.sub,
+                        isAdmin: true, // All users from Azure AD are administrators
+                    };
+                    return newProfile;
+                },
+            })
+        );
+    }
 
-  if (
-      process.env.OKTA_CLIENT_ID &&
-      process.env.OKTA_CLIENT_SECRET &&
-      process.env.OKTA_ISSUER
-  ) {
-    providers.push(
-        OktaProvider({
-          clientId: process.env.OKTA_CLIENT_ID!,
-          clientSecret: process.env.OKTA_CLIENT_SECRET!,
-          issuer: process.env.OKTA_ISSUER!,
-        })
-    );
-  }
+    if (
+        process.env.OKTA_CLIENT_ID &&
+        process.env.OKTA_CLIENT_SECRET &&
+        process.env.OKTA_ISSUER
+    ) {
+        providers.push(
+            OktaProvider({
+                clientId: process.env.OKTA_CLIENT_ID!,
+                clientSecret: process.env.OKTA_CLIENT_SECRET!,
+                issuer: process.env.OKTA_ISSUER!,
+            })
+        );
+    }
 
-  return providers;
+    return providers;
 };
 
 export const options: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [...configureIdentityProvider()],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user?.isAdmin) {
-        token.isAdmin = user.isAdmin;
-      }
-      return token;
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [...configureIdentityProvider()],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user?.isAdmin) {
+                token.isAdmin = user.isAdmin;
+            }
+            return token;
+        },
+        async session({ session, token, user }) {
+            session.user.isAdmin = token.isAdmin as boolean;
+            return session;
+        },
     },
-    async session({ session, token, user }) {
-      session.user.isAdmin = token.isAdmin as boolean;
-      return session;
+    session: {
+        strategy: "jwt",
     },
-  },
-  session: {
-    strategy: "jwt",
-  },
 };
 
 export const handlers = NextAuth(options);
--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -18,27 +18,29 @@ interface LoginProps {
 
 export const LogIn: FC<LoginProps> = (props) => {
   return (
-    <Card className="flex gap-2 flex-col min-w-[300px]">
-      <CardHeader className="gap-2">
-        <CardTitle className="text-2xl flex gap-2">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src={"ai-icon.png"} />
-          </Avatar>
-          <span className="text-primary">{AI_NAME}</span>
-        </CardTitle>
-        <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        {props.isDevMode ? (
-          <Button onClick={() => signIn("localdev")}>
-            Basic Auth (DEV ONLY)
-          </Button>
-        ) : null}
-      </CardContent>
-    </Card>
+      <Card className="flex gap-2 flex-col min-w-[300px]">
+        <CardHeader className="gap-2">
+          <CardTitle className="text-2xl flex gap-2">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src={"ai-icon.png"} />
+            </Avatar>
+            <span className="text-primary">{AI_NAME}</span>
+          </CardTitle>
+          <CardDescription>
+            <b>This model is hosted in the Loyal Source GCC Cloud.</b><br/>
+            All activities within LoyalGPT are subject to monitoring and surveillance. By logging in, you consent to the collection and analysis of your interactions for security purposes. Unauthorized access or misuse may lead to legal consequences. Proceeding beyond this point indicates your acceptance of these terms.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Button onClick={() => signIn("okta")}> Sign in with Okta </Button>
+          <Button onClick={() => signIn("azure-ad")}> Sign in as Admin </Button>
+
+          {props.isDevMode ? (
+              <Button onClick={() => signIn("localdev")}>
+                Basic Auth (DEV ONLY)
+              </Button>
+          ) : null}
+        </CardContent>
+      </Card>
   );
 };
