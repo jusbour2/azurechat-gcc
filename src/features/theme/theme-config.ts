3de0e9287dbd73dc5b9031e6d40024d31a42e501
Justin Harbour <github2@justinharbour.com>
2024-04-22 23:27:50 -0400

    finishing branding

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
-export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
+export const AI_NAME = "LoyalGPT GCC";
+export const AI_DESCRIPTION = "LoyalGPT is a friendly AI assistant designed to keep internal data private and provide access to advanced features.";
+export const CHAT_DEFAULT_PERSONA = AI_NAME + " GPT-4.0";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly AI chatbot. You're here to answer questions and provide assistance. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
